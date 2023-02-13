9e0464173ed76163a83aac66e90599a634d67889
JosueSaenz <josuesaenz885@gmail.com>
2023-02-13 12:00:34 -0500

    Programa Final

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['styles.css']
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
+
+async presentAlert(){
+  const alert = await this.alertController.create({
+header: 'Alerta',
+subHeader: 'Alerta de Ionic',
+message: 'Hola desde Alerta ionic',
+buttons: ['ok','cancel'],
+
+  });
+
+  await alert.present();
+}
+
+
+async presentAlert2(){
+  const alert = await this.alertController.create({
+header: 'Alerta2',
+subHeader: 'Alerta de Ionic 2',
+message: 'Hola desde Alerta ionic 2',
+buttons: ['ok 2','cancel 2'],
+
+  });
+
+  await alert.present();
+}
 
 }
